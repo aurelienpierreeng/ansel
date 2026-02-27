9ace51bf260725734f38283614d16b0a14d4f0cd
Aurélien PIERRE <contact@aurelienpierre.com>
2026-02-27 15:27:15 +0100

    Ellipse: remove false-sharing in outer loops

--- src/develop/masks/ellipse.c
+++ src/develop/masks/ellipse.c
@@ -1150,13 +1150,9 @@ static void _fill_mask(const size_t numpoints, float *const bufptr, const float 
   // be rotated, but we can compensate for that by applying a rotation matrix for the same rotation in the opposite
   // direction before projecting the vector.
 #ifdef _OPENMP
-#if !defined(__SUNOS__) && !defined(__NetBSD__)
 #pragma omp parallel for default(none) \
   dt_omp_firstprivate(numpoints, bufptr, points, center, alpha, a2, b2, ta2, tb2, cos_alpha, sin_alpha, out_scale) \
   schedule(static)
-#else
-#pragma omp parallel for shared(points)
-#endif
 #endif
   for(size_t i = 0; i < numpoints; i++)
     {
@@ -1463,13 +1459,8 @@ static int _ellipse_get_mask_roi(const dt_iop_module_t *const module, const dt_d
   if(ell == NULL) return 1;
 
 #ifdef _OPENMP
-#if !defined(__SUNOS__) && !defined(__NetBSD__)
 #pragma omp parallel for default(none) \
-  dt_omp_firstprivate(ellpts, center, ta, tb, cosa, sina) \
-  shared(ell)
-#else
-#pragma omp parallel for shared(points)
-#endif
+  dt_omp_firstprivate(ellpts, center, ta, tb, cosa, sina, ell)
 #endif
   for(int n = 0; n < ellpts; n++)
   {
@@ -1550,13 +1541,8 @@ static int _ellipse_get_mask_roi(const dt_iop_module_t *const module, const dt_d
 
   // we populate the grid points in module coordinates
 #ifdef _OPENMP
-#if !defined(__SUNOS__) && !defined(__NetBSD__)
 #pragma omp parallel for default(none) \
-  dt_omp_firstprivate(grid, bbxm, bbym, bbXM, bbYM, bbw, iscale, px, py) \
-  shared(points) schedule(static) collapse(2)
-#else
-#pragma omp parallel for shared(points)
-#endif
+  dt_omp_firstprivate(grid, bbxm, bbym, bbXM, bbYM, bbw, iscale, px, py, points)
 #endif
   for(int j = bbym; j <= bbYM; j++)
     for(int i = bbxm; i <= bbXM; i++)
@@ -1610,13 +1596,8 @@ static int _ellipse_get_mask_roi(const dt_iop_module_t *const module, const dt_d
     w1[i] = (float)i;
   }
 #ifdef _OPENMP
-#if !defined(__SUNOS__) && !defined(__NetBSD__)
 #pragma omp parallel for default(none) \
-  dt_omp_firstprivate(grid, bbxm, bbym, bbw, endx, endy, w) \
-  shared(buffer, points)
-#else
-#pragma omp parallel for shared(buffer)
-#endif
+  dt_omp_firstprivate(grid, bbxm, bbym, bbw, endx, endy, w, buffer, points, w0, w1, inv_grid2)
 #endif
   for(int j = bbym * grid; j < endy; j++)
   {
