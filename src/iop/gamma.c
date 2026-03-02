f7726a75c5158bbc7f290a52ee430f272070b08b
Aurélien PIERRE <contact@aurelienpierre.com>
2026-03-02 17:29:38 +0100

    Fix a typo

--- src/iop/gamma.c
+++ src/iop/gamma.c
@@ -305,7 +305,7 @@ static void _mask_display(const float *const restrict in, uint8_t *const restric
 
   #ifdef _OPENMP
   #pragma omp parallel for simd default(none) schedule(static) aligned(in, out: 64) aligned(hidden_color: 16) \
-      dt_omp_firstprivate(in, out, buffsize, alpha, hidden_color)
+      dt_omp_firstprivate(in, out, buffsize, alpha, mask_color)
   #endif
     for(size_t j = 0; j < buffsize; j+= 4)
     {
