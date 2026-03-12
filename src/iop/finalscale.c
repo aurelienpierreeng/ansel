59ed10e559dc2bb261bbddd23e433d450040d789
Aurélien PIERRE <contact@aurelienpierre.com>
2026-03-12 13:07:39 +0100

    Finalscale: disable on anything else than export, explicitely

--- src/iop/finalscale.c
+++ src/iop/finalscale.c
@@ -125,13 +125,14 @@ error:
 void commit_params(dt_iop_module_t *self, dt_iop_params_t *params, dt_dev_pixelpipe_t *pipe,
                    dt_dev_pixelpipe_iop_t *piece)
 {
-  piece->enabled = (piece->pipe->type != DT_DEV_PIXELPIPE_THUMBNAIL);
+  piece->enabled = (piece->pipe->type == DT_DEV_PIXELPIPE_EXPORT);
 }
 
 void init_pipe(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
 {
   piece->data = dt_calloc_align(sizeof(dt_iop_finalscale_data_t));
   piece->data_size = sizeof(dt_iop_finalscale_data_t);
+  piece->enabled = (piece->pipe->type == DT_DEV_PIXELPIPE_EXPORT);
 }
 
 void cleanup_pipe(dt_iop_module_t *self, dt_dev_pixelpipe_t *pipe, dt_dev_pixelpipe_iop_t *piece)
