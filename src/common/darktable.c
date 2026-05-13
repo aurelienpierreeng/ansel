4593b77521da2a7453b80178e2f41dc3b01d964a
Guillaume Stutin <jiyone79@hotmail.fr>
2026-05-13 12:03:06 +0200

    Minor cleanup

--- src/common/darktable.c
+++ src/common/darktable.c
@@ -1472,6 +1472,7 @@ void dt_cleanup()
   if(init_gui)
   {
     g_thread_pool_stop_unused_threads();
+#if !defined(_WIN32) && !defined(__APPLE__)
     PangoFontMap *fontmap = pango_cairo_font_map_get_default();
     gboolean use_fontconfig_backend = FALSE;
 
@@ -1482,14 +1483,11 @@ void dt_cleanup()
       use_fontconfig_backend = (font_backend == CAIRO_FONT_TYPE_FT);
     }
 
-#if !defined(_WIN32) && !defined(__APPLE__)
     if(use_fontconfig_backend && fontmap && g_type_is_a(G_OBJECT_TYPE(fontmap), pango_fc_font_map_get_type()))
       pango_fc_font_map_shutdown((PangoFcFontMap *)fontmap);
-#endif
-    pango_cairo_font_map_set_default(NULL);
-#if !defined(_WIN32) && !defined(__APPLE__)
     if(use_fontconfig_backend) FcFini();
 #endif
+    pango_cairo_font_map_set_default(NULL);
   }
 }
 
