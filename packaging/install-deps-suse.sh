50faf614691a7a247088cbc637216ff069966a57
mattF11 <251583778+mattF11@users.noreply.github.com>
2026-05-31 23:47:40 +0200

    Update install-deps-suse.sh
    fixing errors

--- packaging/install-deps-suse.sh
+++ packaging/install-deps-suse.sh
@@ -23,12 +23,11 @@
 
 ####################################################################################
 #Functions used to install dependancies
+set -euo pipefail
 #1: Tumbleweed function
-function Tumbleweed_function
+function tumbleweed_function
 {
-    set -euo pipefail
-
-    if [ "${EUID:-$(id -u)}" -ne 0 ]
+    if [[ "${EUID:-$(id -u)}" -ne 0 ]]
     then
 	SUDO=(sudo)
     else
@@ -117,15 +116,14 @@ function Tumbleweed_function
 	squashfs
 	update-desktop-files )
     "${SUDO[@]}" zypper --non-interactive install --no-recommends "${ZYPPER_PACKAGES[@]}"
+	return 0
 }
 
 ##########################################################################################
 #2: Leap function
-function Leap_function
+function leap_function
 {
-    set -euo pipefail
-
-    if [ "${EUID:-$(id -u)}" -ne 0 ]
+    if [[ "${EUID:-$(id -u)}" -ne 0 ]]
     then
 	SUDO=(sudo)
     else
@@ -216,26 +214,30 @@ function Leap_function
 	libx265-215
     )
     "${SUDO[@]}" zypper --non-interactive install --no-recommends "${ZYPPER_PACKAGES[@]}"
+	return 0
 }
 
 ########################################################################################
-source /etc/os-release
-distro="$ID"
+#source /etc/os-release
+#distro="$ID"
 #searching the distro name: grep distro id from /etc/os-relase,then cut
 #distro=$(grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '""')
+# distro and ID
+distro=$(grep '^ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+ID="$distro"
 
 # a case should make adding the immutable distros simpler,a function for them as well.
 case "$ID" in
     openSUSE-tumbleweed|opensuse-tumbleweed)
         echo "OpenSUSE Tumbleweed detected"
 	#exec tumbleweed function
-        Tumbleweed_function
+        tumbleweed_function
         exit 0
         ;;
     openSUSE-leap|opensuse-leap)
         echo "OpenSUSE Leap detected"
 	#exec leap function
-        Leap_function
+        leap_function
         exit 0
         ;;
     #if none of them
