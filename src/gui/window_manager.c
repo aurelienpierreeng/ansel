2a5e1b948e1319317516f47290b436138d8f3941
Aurélien PIERRE <contact@aurelienpierre.com>
2026-06-18 00:09:16 +0200

    Fix the slight lighttable misalignement in scrolledwindow

--- src/gui/window_manager.c
+++ src/gui/window_manager.c
@@ -532,7 +532,7 @@ void dt_ui_init_main_table(GtkWidget *parent, dt_ui_t *ui)
   _ui_init_panel_right(ui, container);
 
   /* initialize the main drawing widget (center) */
-  widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, DT_GUI_BOX_SPACING);
+  widget = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   gtk_widget_set_name(widget, "main-widget");
   gtk_widget_set_hexpand(GTK_WIDGET(widget), TRUE);
   gtk_widget_set_vexpand(GTK_WIDGET(widget), TRUE);
